d9b435deff4d43da891dccc82ff6c1ce0859ff60
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-06-14 16:16:52 +0600

    create model

--- quick_find_movies/core/models.py
+++ quick_find_movies/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    year = models.IntegerField()
+    director = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
